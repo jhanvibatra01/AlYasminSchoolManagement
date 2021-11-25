c0085046e21366acfab86af8b74448f03eb1d0b0
jhanvibatra01 <43892827+jhanvibatra01@users.noreply.github.com>
2021-11-25 14:56:10 +0530

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,32 @@
+package com.tw;
+
+
+public class Student {
+    private String name;
+    private int rollNo;
+
+    Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getRollNo() {
+        return rollNo;
+    }
+
+    void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String toString() {
+        return this.rollNo + ". " + this.name;
+    }
+}
